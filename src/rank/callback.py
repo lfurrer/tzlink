bc2e9e03122ef069efba16a3a4475eb68e0abab1
Lenz <Lenz.Furrer@gmail.com>
2018-06-05 17:27:13 +0300

    fix and integrate early-stopping callback

--- src/rank/callback.py
+++ src/rank/callback.py
@@ -1,49 +1,58 @@
+#!/usr/bin/env python3
+# coding: utf8
 
-""" An early-stopping callback based on ranking accuracy
-evaluator = Evaluator()
-1. evaluator returns accuracy by evaluator.accuracy
-2. the callback function somehow
-	stores the accuracy at the beginning of each epoch?
-	terminates early if accuracy does not improve in a
-        given number of epochs
-3. what else do we need? do we need loss?
+
+"""
+An early-stopping callback based on ranking accuracy.
 """
 
-import keras
 
-class ranking_accuracy_callback(keras.callbacks.Callback):
-    '''A callback function based on ranking accuracy.
-    Modified from keras EarlyStopping callback
+import numpy as np
+from keras.callbacks import Callback
+
+from .predictions import Evaluator
+
+
+class EarlyStoppingRankingAccuracy(Callback):
+    '''Stop training when ranking accuracy has stopped improving.
+
+    Based on keras.callbacks.EarlyStopping.
 
     #Arguments
-        evaluator: the evaluator object whose accuracy is
-            measured
+        conf: a conf.config.Config instance with settings.
+        val_data: a preprocessing.samples.DataSet instance
+            with validation data.
         min_delta: minimum change in the monitored quantity
             to qualify as an improvement.
-        patience: number of epochs with no improvement,
-            default = 0
-        after whichtraining will be stopped.
+        patience: number of epochs with no improvement
+            after which training will be stopped.
         verbose: verbosity mode.
-        baseline: Baseline value for the monitored quantity
+        baseline: Baseline value for ranking accuracy
             to reach. Training will stop if the model does
             not show improvement over the baseline.
     '''
     def __init__(self,
-                 evaluator,
+                 conf,
+                 val_data,
+                 min_delta=0,
                  patience=0,
                  verbose=0,
                  baseline=None):
-    super(EarlyStopping, self).__init__()
+        super().__init__()
 
-    self.evaluator = evaluator
-    self.patience = patience
-    self.verbose = verbose
-    self.baseline = baseline
-    self.min_delta = 1
-    self.wait = 0
-    self.stopped_epoch = 0
+        self.conf = conf
+        self.val_data = val_data
 
-    def on_train_begin(self, logs={}):
+        self.patience = patience
+        self.verbose = verbose
+        self.baseline = baseline
+        self.min_delta = min_delta
+
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.best = None
+
+    def on_train_begin(self, logs=None):
         # Allow instances to be re-used
         self.wait = 0
         self.stopped_epoch = 0
@@ -51,14 +60,9 @@ class ranking_accuracy_callback(keras.callbacks.Callback):
             self.best = self.baseline
         else:
             self.best = -np.Inf
-        self.accuracy = []
 
-    #def on_epoch_begin(self, logs={}):
-    #    return
- 
-    def on_epoch_end(self, epoch, logs={}):
-        self.accuracy.append(getattr(evaluator, accuracy))
-        current = logs.get(self.accuracy)
+    def on_epoch_end(self, epoch, logs=None):
+        current = self.evaluate()
         if np.greater(current - self.min_delta, self.best):
             self.best = current
             self.wait = 0
@@ -68,12 +72,15 @@ class ranking_accuracy_callback(keras.callbacks.Callback):
                 self.stopped_epoch = epoch
                 self.model.stop_training = True
 
-    #def on_batch_begin(self, batch, logs={}):
-    #    return
-
-    #def on_batch_end(self, batch, logs={}):
-    #    return
-
     def on_train_end(self, logs=None):
         if self.stopped_epoch > 0 and self.verbose > 0:
             print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+
+    def evaluate(self):
+        '''
+        Compute accuracy with the current model.
+        '''
+        self.val_data.scores = self.model.predict(
+            self.val_data.x, batch_size=self.conf.rank.batch_size)
+        accuracy = Evaluator.from_data(self.conf, self.val_data).accuracy
+        return accuracy
