e9515d0527a9d68dfa1107cdeffdfda5c33955e5
Lenz <Lenz.Furrer@gmail.com>
2018-06-05 17:43:51 +0300

    configure the early-stopping callback through the omnipresent conf object

--- src/rank/callback.py
+++ src/rank/callback.py
@@ -7,7 +7,8 @@ An early-stopping callback based on ranking accuracy.
 """
 
 
-import numpy as np
+import logging
+
 from keras.callbacks import Callback
 
 from .predictions import Evaluator
@@ -22,31 +23,14 @@ class EarlyStoppingRankingAccuracy(Callback):
         conf: a conf.config.Config instance with settings.
         val_data: a preprocessing.samples.DataSet instance
             with validation data.
-        min_delta: minimum change in the monitored quantity
-            to qualify as an improvement.
-        patience: number of epochs with no improvement
-            after which training will be stopped.
-        verbose: verbosity mode.
-        baseline: Baseline value for ranking accuracy
-            to reach. Training will stop if the model does
-            not show improvement over the baseline.
     '''
     def __init__(self,
                  conf,
-                 val_data,
-                 min_delta=0,
-                 patience=0,
-                 verbose=0,
-                 baseline=None):
+                 val_data):
         super().__init__()
 
         self.conf = conf
         self.val_data = val_data
-
-        self.patience = patience
-        self.verbose = verbose
-        self.baseline = baseline
-        self.min_delta = min_delta
 
         self.wait = 0
         self.stopped_epoch = 0
@@ -56,25 +40,23 @@ class EarlyStoppingRankingAccuracy(Callback):
         # Allow instances to be re-used
         self.wait = 0
         self.stopped_epoch = 0
-        if self.baseline is not None:
-            self.best = self.baseline
-        else:
-            self.best = -np.Inf
+        self.best = self.conf.stop.baseline
 
     def on_epoch_end(self, epoch, logs=None):
         current = self.evaluate()
-        if np.greater(current - self.min_delta, self.best):
+        logging.info('Ranking accuracy: %g', current)
+        if current - self.conf.stop.min_delta > self.best:
             self.best = current
             self.wait = 0
         else:
             self.wait += 1
-            if self.wait >= self.patience:
+            if self.wait >= self.conf.stop.patience:
                 self.stopped_epoch = epoch
                 self.model.stop_training = True
 
     def on_train_end(self, logs=None):
-        if self.stopped_epoch > 0 and self.verbose > 0:
-            print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+        if self.stopped_epoch > 0:
+            logging.info('Epoch %05d: early stopping', self.stopped_epoch + 1)
 
     def evaluate(self):
         '''
