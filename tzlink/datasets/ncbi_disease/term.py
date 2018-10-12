a70a5db23b8d8c1a583bb6db89cd9664ac1e7737
Lenz <Lenz.Furrer@gmail.com>
2018-10-12 10:07:50 +0200

    refactor: make room for additional preprocessing steps
    includes two fixes:
    - if there were no synonyms before, don't add a leading pipe
    - if a UMLS concept maps to multiple entries, add the new synonyms to all of them

--- tzlink/datasets/ncbi_disease/term.py
+++ tzlink/datasets/ncbi_disease/term.py
@@ -16,6 +16,11 @@ from ...util.util import smart_open
 from ...util.umls import read_RRF, iterconcepts, C
 
 
+# Column IDs of the MEDIC TSV.
+ALT = 2
+SYN = 7
+
+
 def parse_MEDIC_terminology(source):
     '''
     Parse the MEDIC TSV into an iterator of namedtuples.
@@ -27,7 +32,7 @@ def parse_MEDIC_terminology(source):
             yield from _parse_MEDIC_terminology(f)
 
 
-def _parse_MEDIC_format(file):
+def _parse_MEDIC_terminology(file):
     '''
     Input fields:
         DiseaseName
@@ -46,23 +51,30 @@ def _parse_MEDIC_format(file):
         def_ (str):  definition (if any)
         syn (tuple): synonyms (if any)
     '''
+    for name, id_, alt, def_, _, _, _, syn in _parse_MEDIC_format(file):
+        yield DictEntry(name, id_, alt, def_, syn)
+
+
+def _parse_MEDIC_format(file, skip_comments=True):
     reader = csv.reader(file, delimiter='\t', quotechar=None)
     for row in reader:
         if row[0].startswith('#'):
-            continue
-        name, id_, alt, def_, _, _, _, syn = row
-        yield name, id_, alt, def_, syn
+            if skip_comments:
+                continue
+        else:
+            _unpack_list_value_fields(row)
+        yield row
 
 
-def _parse_MEDIC_terminology(file):
-    for name, id_, alt, def_, syn in _parse_MEDIC_format(file):
-        yield DictEntry(
-            name,
-            id_,
-            tuple(alt.split('|')) if alt else (),
-            def_,
-            tuple(syn.split('|')) if syn else (),
-        )
+def _unpack_list_value_fields(row):
+    '''Break up the list-value fields "alt" and "syn".'''
+    for i in (ALT, SYN):
+        row[i] = tuple(row[i].split('|') if row[i] else ())
+
+
+def _pack_list_value_fields(row):
+    for i in (ALT, SYN):
+        row[i] = '|'.join(row[i])
 
 
 def extend_MEDIC_with_UMLS(medic, metadir, target):
@@ -74,79 +86,119 @@ def extend_MEDIC_with_UMLS(medic, metadir, target):
 
 
 def _extend_MEDIC_with_UMLS(medic, metadir, target):
-    concepts = iterconcepts(read_RRF(metadir, 'CONSO'))
-    with MedicBuffer(medic, target) as mbuf:
-        for conc in concepts:
-            ids = list(_medic_ids(conc))
-            if not ids:
-                continue
-            names = (r[C.STR] for r in conc)
-            mbuf.update(ids, names)
+    lines = list(_parse_MEDIC_format(medic, skip_comments=False))
+    writer = csv.writer(target, delimiter='\t', quotechar=None)
 
-
-def _medic_ids(conc):
-    for row in conc:
+    # Get rid of the header lines right away.
+    while lines[0][0].startswith('#'):
+        writer.writerow(lines.pop(0))
+    # Apply all modifications in-place.
+    _add_UMLS_names(lines, metadir)
+    # Write the modified terminology to disk.
+    for row in lines:
         try:
-            prefix = _prefixes[row[C.SAB]]
-        except KeyError:
-            pass
-        else:
-            yield prefix + row[C.CODE]
-
-_prefixes = {'MSH': 'MESH:', 'OMIM': 'OMIM:'}
+            _pack_list_value_fields(row)
+        except IndexError:  # mid-file comments
+            pass  # no packing needed
+        writer.writerow(row)
 
 
-class MedicBuffer:
+class _MedicBuffer:
     '''
-    Keep the MEDIC vocabulary in memory while adding synonyms.
-    '''
-    def __init__(self, infile, outfile):
-        self._index = {}
-        self._lines = []
-        self._outfile = outfile
-        self._read(infile)
+    Abstract base class for UMLS-extension filters.
 
-    def _read(self, infile):
-        _D = {}  # reused empty dummy dict
-        for line in infile:
-            line = line.rstrip('\n\r')
-            if line.startswith('#'):
-                self._lines.append((line, _D))
+    Subclasses are used for modifying MEDIC lines in-place
+    while applying UMLS-based extensions.
+
+    The filters are implemented as classes for reasons of
+    inheritance and shared state, but are to be used like
+    functions with side effect.
+    '''
+    def __init__(self, lines, metadir):
+        self._lines = lines
+        self._concepts = iterconcepts(read_RRF(metadir, 'CONSO'))
+
+        self._setup()
+        self._run()
+        self._flush()
+
+    def _setup(self):
+        raise NotImplementedError
+
+    def _run(self):
+        for conc in self._concepts:
+            ids = self._medic_ids(conc)
+            self._use(ids, conc)
+
+    def _use(self, ids, conc):
+        raise NotImplementedError
+
+    def _flush(self):
+        '''
+        Propagate the changes to the original lines.
+        '''
+        raise NotImplementedError
+
+    def _iterlines(self):
+        '''
+        Iterate over lines, skipping mid-file comments.
+        '''
+        for line in self._lines:
+            if not line[0].startswith('#'):
+                yield line
+
+    @classmethod
+    def _medic_ids(cls, conc):
+        for row in conc:
+            try:
+                prefix = cls._prefixes[row[C.SAB]]
+            except KeyError:
+                pass
             else:
-                self._parse(line)
+                yield prefix + row[C.CODE]
 
-    def _parse(self, line):
-        name, id_, alt, _, _, _, _, syn = line.split('\t')
-        syn, alt = ((s.split('|') if s else ()) for s in (syn, alt))
-        names = {n.lower(): None for n in (name, *syn)}
-        self._lines.append((line, names))
-        for i in (id_, *alt):
-            self._index.setdefault(i, []).append(names)
+    _prefixes = {'MSH': 'MESH:', 'OMIM': 'OMIM:'}
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-        return False
+class _add_UMLS_names(_MedicBuffer):
+    '''
+    Add synonyms to the MEDIC vocabulary.
+    '''
+    def __init__(self, lines, metadir):
+        self._index = {}
+        self._names = []
+        super().__init__(lines, metadir)
 
-    def update(self, ids, newnames):
+    def _setup(self):
+        for line in self._iterlines():
+            self._add_line(line)
+
+    def _add_line(self, line):
+        try:
+            name, id_, alt, _, _, _, _, syn = line
+        except ValueError:
+            names = {}
+        else:
+            names = {n.lower(): None for n in (name, *syn)}
+            for i in (id_, *alt):
+                self._index.setdefault(i, []).append(names)
+        self._names.append(names)
+
+    def _use(self, ids, conc):
+        names = [r[C.STR] for r in conc]
+        for id_ in ids:
+            self._add_synonyms(id_, names)
+
+    def _add_synonyms(self, id_, synonyms):
         '''
-        Add new synonyms to all rows that match the IDs.
+        Add new synonyms to all rows that match this ID.
         '''
-        for i in ids:
-            for names in self._index.get(i, ()):
-                for n in newnames:
-                    names.setdefault(n.lower(), n)
+        for names in self._index.get(id_, ()):
+            for n in synonyms:
+                names.setdefault(n.lower(), n)
 
-    def close(self):
-        '''
-        Write the modified file to disk.
-        '''
-        for line, names in self._lines:
-            newnames = [n for n in names.values() if n is not None]
-            self._outfile.write(line)
+    def _flush(self):
+        for line, names in zip(self._iterlines(), self._names):
+            newnames = tuple(n for n in names.values() if n is not None)
             if newnames:
-                self._outfile.write('|')
-                self._outfile.write('|'.join(newnames))
-            self._outfile.write('\n')
+                line[SYN] += newnames
